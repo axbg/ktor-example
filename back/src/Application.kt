ef1158dc5402d7d51acf9c929f405cadb46aaebf
axbg <bisagalexstefan@gmail.com>
2019-09-28 23:37:16 +0300

    Initialized ktor backend

---
+++ back/src/Application.kt
@@ -0,0 +1,76 @@
+package com.axbg.ctd
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
+import io.ktor.client.features.logging.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(Authentication) {
+    }
+
+    val client = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        install(Logging) {
+            level = LogLevel.HEADERS
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
+data class JsonSampleClass(val hello: String)
+
