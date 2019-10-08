36564c0b6cc4f6778cbcefbc61716fce34038ea8
axbg <bisagalexstefan@gmail.com>
2019-10-08 23:59:08 +0300

    Implemented JWT Login

--- back/src/Application.kt
+++ back/src/Application.kt
@@ -4,21 +4,20 @@ import com.axbg.ctd.config.AppException
 import com.axbg.ctd.config.Constants
 import com.axbg.ctd.controllers.taskController
 import com.axbg.ctd.controllers.userController
-import com.axbg.ctd.models.DatabaseModel
+import com.axbg.ctd.models.*
 import com.axbg.ctd.services.TaskService
 import com.axbg.ctd.services.TaskServiceImpl
 import com.axbg.ctd.services.UserService
 import com.axbg.ctd.services.UserServiceImpl
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.auth.*
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.request.get
-import io.ktor.client.request.header
 import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.StatusPages
@@ -26,11 +25,12 @@ import io.ktor.gson.gson
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.route
-import io.ktor.routing.routing
+import io.ktor.routing.post
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -53,34 +53,30 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
-    install(Authentication) {
-        oauth("google-oauth") {
-            client = HttpClient(Apache)
-            providerLookup = { Constants.googleOauthProvider }
-            urlProvider = { "http://localhost:8080/login"}
-        }
-    }
-
-    routing {
-
-    }
-
     install(Routing) {
         get("/") {
             call.respond(HttpStatusCode.OK, mapOf("message" to "hourglass back-end"))
         }
 
-        authenticate("google-oauth") {
-            route("/login") {
-                handle {
-                    val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
-                        ?: error("No principal")
+        post("/login") {
+            val googleToken: GoogleToken = call.receive()
+            val googleResponse = HttpClient(Apache).get<String>(Constants.googleOauthCheck + googleToken.token)
+            val decodedResponse = Gson().fromJson<GoogleData>(googleResponse, GoogleData::class.java)
 
-                    val json = HttpClient(Apache).get<String>("https://www.googleapis.com/userinfo/v2/me") {
-                        header("Authorization", "Bearer $(principal.accessToken)")
-                    }
+            var user: User? = null
 
+            transaction {
+                user = User.find { Users.mail eq decodedResponse.email }.firstOrNull()
+            }
 
+            when {
+                user != null -> {
+                    val key = Keys.hmacShaKeyFor(Constants.privateKey.toByteArray())
+                    val token = Jwts.builder().setSubject(user!!.id.toString()).signWith(key).compact()
+                    call.respond(mapOf("token" to token))
+                }
+                else -> {
+                    throw AppException("User not found", 404)
                 }
             }
         }
--- back/src/config/Constants.kt
+++ back/src/config/Constants.kt
@@ -8,15 +8,7 @@ object Constants {
     const val DATABASE_DRIVER: String = "com.mysql.cj.jdbc.Driver"
     const val DATABASE_USERNAME: String = "alex"
     const val DATABASE_PASSWORD: String = "forever"
-    val googleOauthProvider = OAuthServerSettings.OAuth2ServerSettings(
-        name = "google",
-        authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-        accessTokenUrl = "https://www.googleapis.com/oauth2/v3/token",
-        requestMethod = HttpMethod.Post,
-
-        clientId = "xxxxxxxxxxx.apps.googleusercontent.com",
-        clientSecret = "yyyyyyyyyyy",
-        defaultScopes = listOf("profile") // no email, but gives full name, picture, and id
-    )
+    const val googleOauthCheck = "https://oauth2.googleapis.com/tokeninfo?id_token="
+    const val privateKey = "ejrijeoirjbeiorjoigj2345o24mgbioemrobemrihmio345"
 }
 
